6a6c0881724c1dde28c3ba104bfd4849cc81286e
Alok Maurya <106429374+alokmaurya013@users.noreply.github.com>
2024-02-21 18:57:39 +0530

    Time: 10 ms (41.24%), Space: 8.8 MB (26.31%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        if(left==0){
+            return 0;
+        }
+        if(__builtin_clz(left)!=__builtin_clz(right)){
+            return 0;
+        }
+        while(right>left){
+            right&=(right-1);
+        }
+         return right;
+    }
+};
