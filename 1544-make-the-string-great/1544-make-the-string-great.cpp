b578a68eb3ee924617a319390ac1f89648edd6b8
Alok Maurya <106429374+alokmaurya013@users.noreply.github.com>
2024-04-06 00:04:08 +0530

    Time: 5 ms (30.95%), Space: 8 MB (49.20%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    char a[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    string makeGood(string s) {
+        int n=s.length(),isupper=0;
+        for(int i=0;i<n;i++){
+            if(s[i]>='A'&&s[i]<='Z'){
+                isupper=1;
+                break;
+            }
+        }
+        if(n==1||isupper==0){
+            return s;
+        }
+        stack<char>st;
+        int i=0;
+        st.push(s[i]);
+        i++;
+        while(i<n){
+            if(st.empty()){
+                st.push(s[i]);
+            }else{
+            char t=st.top();
+        
+            if(t>='a'&&t<='z'){
+                if(s[i]==a[t-'a']){
+                    st.pop();
+                }else{
+                    st.push(s[i]);
+                }
+            }else{
+                if(t==(s[i]-'a'+'A')){
+                    st.pop();
+                }else{
+                    st.push(s[i]);
+                }
+            }
+            }
+            i++;
+        }
+        string t="";
+        while(!st.empty()){
+            t+=st.top();
+            st.pop();
+        }
+        reverse(t.begin(),t.end());
+        return t;
+    }
+};
