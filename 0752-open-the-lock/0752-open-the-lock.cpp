305bc945436e17ce9cfcdb78e5dc3bcf51914f16
Alok Maurya <106429374+alokmaurya013@users.noreply.github.com>
2024-04-22 19:10:58 +0530

    Time: 99 ms (87.87%), Space: 25.3 MB (89.84%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    int openLock(vector<string>& deadends, string target) {
+      vector<int>vis(10000,0);
+       int n=deadends.size();
+        queue<pair<int,string>>q;
+        for(auto i:deadends){
+            vis[stoi(i)]=1;
+        }
+        if(vis[0]){
+            return -1;
+        }
+        q.emplace(0,"0000");
+        vis[0]=1;
+        while(!q.empty()){
+            auto [turn,s]=q.front();
+            q.pop();
+
+            if(s==target){
+                return turn;
+            }
+            for(int i=0;i<4;i++){
+                for(auto j:{-1,1}){
+                    string t=s;
+                    char& ch=t[i];
+                    ch=(ch-'0'+j+10)%10+'0';
+                    int tmp=stoi(t);
+                    if(!vis[tmp]){
+                        vis[tmp]=1;
+                        q.emplace(turn+1,t);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
