300945f4810ff13249e90745f90481e3d231433d
Alok Maurya <106429374+alokmaurya013@users.noreply.github.com>
2024-05-12 06:55:33 +0530

    Time: 8 ms (72.73%), Space: 13.5 MB (53.31%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int mxValue(vector<vector<int>>&temp){
-        int mx=0;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                mx=max(mx,temp[i][j]);
-            }
-        }
-        return mx;
-    }
+  
     vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
         int n=grid.size();
-        vector<vector<int>>v;
-        vector<vector<int>>temp(3,vector<int>(3,0));
         for(int r=0;r<n-2;r++){
-            vector<int>tmp;
             for(int c=0;c<n-2;c++){
         for(int i=r;i<r+3;i++){
             for(int j=c;j<c+3;j++){
-                temp[i-r][j-c]=grid[i][j];
+                grid[r][c]=max(grid[r][c],grid[i][j]);
             }
         }
-         tmp.push_back(mxValue(temp));
-        }v.push_back(tmp);
+        }
+        grid[r].resize(n-2);
        }
-        return v;
+        grid.resize(n-2);
+        return grid;
     }
 };
