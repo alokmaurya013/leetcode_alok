4503658679d2c8afaf2941d48475a404056b2fb9
Alok Maurya <106429374+alokmaurya013@users.noreply.github.com>
2024-05-12 06:58:52 +0530

    Time: 7 ms (89.68%), Space: 13.6 MB (53.31%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
@@ -5,11 +5,13 @@ public:
         int n=grid.size();
         for(int r=0;r<n-2;r++){
             for(int c=0;c<n-2;c++){
+                int mx=grid[r][c];
         for(int i=r;i<r+3;i++){
             for(int j=c;j<c+3;j++){
-                grid[r][c]=max(grid[r][c],grid[i][j]);
+                mx=max(mx,grid[i][j]);
             }
         }
+        grid[r][c]=mx;
         }
         grid[r].resize(n-2);
        }
