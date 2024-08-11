c6790ca1876e7a9d32d66c1b55a6fca3c96c43b1
Alok Maurya <106429374+alokmaurya013@users.noreply.github.com>
2024-08-11 15:16:55 +0530

    Time: 108 ms (66.67%), Space: 74 MB (41.67%) - LeetHub

---
+++ 3250-find-the-count-of-monotonic-pairs-i/3250-find-the-count-of-monotonic-pairs-i.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+    int cp(vector<int>& nums) {
+        const int MODULO = 1e9 + 7;
+        vector<int> prefix_sum(nums[0] + 1, 1);
+        partial_sum(prefix_sum.begin(), prefix_sum.end(), prefix_sum.begin());
+        
+        auto get = [&](int k) -> int {
+            return k > -1 ? prefix_sum[k] : 0;
+        };
+        
+        int ans = nums[0] + 1;
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            vector<int> tmp = {0};
+            for (int j = 0; j <= nums[i]; ++j) {
+                int sum_val = tmp.back() + get(min(0, nums[i-1] - nums[i]) + j);
+                tmp.push_back(sum_val % MODULO);
+            }
+            prefix_sum.assign(tmp.begin() + 1, tmp.end());
+            ans = tmp.back();
+        }
+        
+        return ans % MODULO;
+    }
+
+    int solve(vector<int>& nums,int n) {
+        const int MOD = 1e9 + 7;
+        vector<vector<int>> dp(n, vector<int>(1001, 0));
+        vector<int> sum(51, 0);
+    
+        for (int i= 0; i<= nums[0];i++) {
+            dp[0][i] = 1;
+            sum[i] = 1;
+        }
+        for (int i = 1; i < n; ++i) {
+            vector<int> new_sum(51, 0);
+            for (int j = 0; j <= nums[i];j++) {
+                dp[i][j] = sum[j];
+                if (j> 0) {
+                    new_sum[j] = (new_sum[j-1] + dp[i][j]) % MOD;
+                } else {
+                    new_sum[j] = dp[i][j];
+                }
+            }
+            sum = new_sum;
+        }
+        int ans=0;
+        for (int i= 0; i<= nums[n-1];i++) {
+            ans= (ans + dp[n-1][i]) % MOD;
+        }
+        
+        return ans;
+    }
+    int countOfPairs(vector<int>& nums) {
+        int n=nums.size();
+       return cp(nums);
+    }
+};
