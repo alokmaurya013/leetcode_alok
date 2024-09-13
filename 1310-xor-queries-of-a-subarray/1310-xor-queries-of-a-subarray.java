98e1d0172bbcf18a4ed08d2aef7b47fc61ef0140
Alok Maurya <106429374+alokmaurya013@users.noreply.github.com>
2024-09-13 08:55:39 +0530

    Time: 2 ms (100.00%), Space: 57.7 MB (55.23%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -1,14 +1,13 @@
 class Solution {
-public:
-    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        int n=arr.size();
-        vector<int>pre(n);
+    public int[] xorQueries(int[] arr, int[][] queries) {
+       int n=arr.length;
+        int[] pre=new int[n];
         pre[0]=arr[0];
         for(int i=1;i<n;i++){
             pre[i]=arr[i]^pre[i-1];
         }
-        int k=queries.size();
-        vector<int>ans(k);
+        int k=queries.length;
+        int[] ans=new int[k];
         for(int i=0;i<k;i++){
             int l=queries[i][0];
             int r=queries[i][1];
@@ -20,4 +19,4 @@ public:
         }
         return ans;
     }
-};
+}
